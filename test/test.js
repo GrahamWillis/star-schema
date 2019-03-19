e28640fc159937604be5fb13ec319df03573b190
graham-willis-druid <graham.willis.ea@gmail.com>
2019-03-19 14:57:58 +0000

    Unit tests

--- test/test.js
+++ test/test.js
@@ -9,7 +9,7 @@ describe('Test star functions', function () {
   before(() => {
   })
 
-  describe('#badDimensions', function () {
+  describe('Bad dimensions', function () {
     it('No array', () => {
       assert.throws(() => {
         new Dimension('id', null)
@@ -23,7 +23,7 @@ describe('Test star functions', function () {
     })
   })
 
-  describe('#badFacts', function () {
+  describe('Bad facts', function () {
     it('No array', () => {
       assert.throws(() => {
         new Fact(null)
@@ -32,8 +32,39 @@ describe('Test star functions', function () {
 
     it('Incorrect dimension set', () => {
       assert.throws(() => {
-        new Fact([], { foo: 'bar' })
+        new Fact([ { id: 1 }], { foo: 'bar' })
       })
+    })
+  })
+
+  describe('Searches', () => {
+    const fact = new Fact([ { id1: 1, id2: 2 }, { id1: 1, id2: 3 } ], {
+      id1: new Dimension('id', [ { id: 1 }, { id: 2 }, { id: 3 } ]),
+      id2: new Dimension('id', [ { id: 1 }, { id: 2 }, { id: 3 } ])
+    })
+
+    it('Search retuning zero results', () => {
+      const results = fact.search({
+        id1: 1,
+        id2: 1
+      })
+      assert.strictEqual(results.length, 0)
+    })
+
+    it('Search retuning one result', () => {
+      const results = fact.search({
+        id1: 1,
+        id2: 2
+      })
+      assert.strictEqual(results.length, 1)
+    })
+
+    it('Search retuning all results', () => {
+      const results = fact.search({
+        id1: 1,
+        id2: [2, 3]
+      })
+      assert.strictEqual(results.length, 2)
     })
   })
 })
