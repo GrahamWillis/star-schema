6afc912f936926531db0cca5625a63920e5254f8
graham-willis-druid <graham.willis.ea@gmail.com>
2019-03-21 09:55:21 +0000

    Added timing to particle example

--- example/particles/particle-example.js
+++ example/particles/particle-example.js
@@ -20,57 +20,87 @@ const subtypeArr = require('./properties/fundamental-subtype')
 
 const dimensions = {}
 
-console.log('Assigning dimensions')
 dimensions.charge = new Dimension('id', chargeArr)
 dimensions.spin = new Dimension('id', spinArr)
 dimensions.particle = new Dimension('id', particleArr)
 dimensions.type = new Dimension('id', typeArr)
 dimensions.subtype = new Dimension('id', subtypeArr)
 
+const runWithTimer = (fnc, t) => {
+  const hrstart = process.hrtime()
+  fnc()
+  const hrend = process.hrtime(hrstart)
+  console.info('Execution time: %ds %dms', hrend[0], hrend[1] / 1000000)
+}
+
 /**
  * Set up the fact (measurement) table. The first argument is the large measurement array
  * and the second argument is an object which maps each row of that array to the dimensions
  **/
-const factArr = require('./particle-fact')
-const Fact = require('../../lib/fact')
 
-console.log('Creating fact table')
-const fact = new Fact(factArr, {
-  particleId: dimensions.particle,
-  chargeId: dimensions.charge,
-  fundamentalSubtype: dimensions.subtype,
-  fundamentalType: dimensions.type,
-  spin: dimensions.spin })
+let factArr
+let Fact
 
+console.log('\nReading data from disk...')
+runWithTimer(() => {
+  factArr = require('./particle-fact')
+  Fact = require('../../lib/fact')
+})
+
+console.log('\nCreating fact table...')
+let fact
+runWithTimer(() => {
+  fact = new Fact(factArr, {
+    particleId: dimensions.particle,
+    chargeId: dimensions.charge,
+    fundamentalSubtype: dimensions.subtype,
+    fundamentalType: dimensions.type,
+    spin: dimensions.spin
+  })
+  console.log(`Created ${fact.index.length} records`)
+})
 /**
  * We can now access various searches on the data as follows
  **/
 
 // Get all measurements of fermions spin one-half
-console.log('Simple search')
-let result = fact.search({
-  fundamentalType: 1,
-  spin: 4
+console.log('\nsearch: fermions spin one-half')
+runWithTimer(() => {
+  let result = fact.search({
+    fundamentalType: 1,
+    spin: 4
+  })
+  console.log(`Found ${result.length} records`)
 })
-console.log(`Found ${result.length} records`)
 
 // Find measurements for all electron, muon and tau neutrinos
-console.log('Compound search 1')
-result = fact.search({
-  particleId: [1, 2, 3]
+console.log('\nsearch: electron, muon and tau neutrinos')
+runWithTimer(() => {
+  let result = fact.search({
+    particleId: [1, 2, 3]
+  })
+  console.log(`Found ${result.length} records`)
 })
-console.log(`Found ${result.length} records`)
 
 // All zero charge and a spin of zero or 1
-console.log('Compound search 2')
-result = fact.search({
-  charge: 1,
-  spin: [2, 6]
+console.log('\nsearch: zero charge and a spin of zero or 1')
+runWithTimer(() => {
+  let result = fact.search({
+    charge: 1,
+    spin: [2, 6]
+  })
+  console.log(`Found ${result.length} records`)
 })
-console.log(`Found ${result.length} records`)
 
-// Example: Calculate the average displacement of particles from the origin
-const av = (r) => r.reduce((a, c) =>
-  Math.pow(Math.pow(c.xposition, 2) + Math.pow(c.yposition, 2) + Math.pow(c.zposition, 2), 0.5) + a, 0) / r.length
+// Example: Calculate the average displacement of all electrons from the origin
+console.log('\nevaluate: average displacement of all electrons from the origin')
+runWithTimer(() => {
+  const av = (r) => r.reduce((a, c) =>
+    Math.pow(Math.pow(c.xposition, 2) + Math.pow(c.yposition, 2) + Math.pow(c.zposition, 2), 0.5) + a, 0) / r.length
 
-console.log(`Average displacement ${av(result)}`)
+  let result = fact.search({
+    particle: 4
+  })
+
+  console.log(`Average displacement of electrons = ${av(result)}`)
+})
